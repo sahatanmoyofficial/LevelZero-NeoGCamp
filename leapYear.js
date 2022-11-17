4737e873b9ac5dd60af0ee10cdfd5bdb27780ad3
Tanmoy Saha <sahatanmoyofficial@gmail.com>
2022-11-17 09:15:06 +0000

    feat: added chalk

--- leapYear.js
+++ leapYear.js
@@ -1,20 +1,20 @@
 import readlineSync from 'readline-sync'
-import moment from 'moment'
+import chalk from 'chalk'
 //import expressValidator from 'expressValidator'
 //var readlineSync = require('readline-sync')
 
 var askName = "Hey there! Thanks for stopping by. May I know your name? "
-var userName = readlineSync.question(askName)
+var userName = readlineSync.question(chalk.green(askName))
 var greeting = "Nice to meet you " + userName
 console.log("")
-console.log(greeting)
+console.log(chalk.magenta(greeting))
 console.log("")
 
 var askDOB = "Please input your date of birth in the following format(dd/mm/yyyy): "
-var userDOB = readlineSync.question(askDOB)
-var validDate = "Please enter a valid date."
-var leapYear = "Congrats it's a leap year"
-var notALeapYear = "It's not a leap year"
+var userDOB = readlineSync.question(chalk.blue(askDOB))
+//var validDate = "Please enter a valid date."
+var leapYear = " is a leap year"
+var notALeapYear = " is not a leap year"
 var start = "The year you have entered"
 var correctDateFlag = false
 var leapYearCheckFlag = false
@@ -23,9 +23,9 @@ var leapYearCheckFlag = false
 function checkValidDate(parameterOne) {
 
     if ((0 == parameterOne % 4) && (0 != parameterOne % 100) || (0 == parameterOne % 400)) {
-        console.log(start + ' is a leap year');
+        console.log(chalk.yellow(start + leapYear));
     } else {
-        console.log(start + ' is not a leap year');
+        console.log(chalk.cyan(start + notALeapYear));
     }
 }
 
