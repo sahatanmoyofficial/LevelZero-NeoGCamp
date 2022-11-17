bd9fa84e3f91cbe56b1aa4ca2e53dee6957a1b66
Tanmoy Saha <sahatanmoyofficial@gmail.com>
2022-11-17 09:20:39 +0000

    feat: added code for pattern matching

---
+++ patternMatching.js
@@ -0,0 +1,50 @@
+//Pattern Matching:--
+
+for (var i = 1; i <= 5; i = i + 1) {
+  var str = "";
+  for (var j = 0; j < i; j = j + 1) {
+
+    str += "*";
+  }
+  console.log(str)
+}
+
+console.log("______________________")
+console.log("______________________")
+
+for(var i=5; i>=1; i--)
+{
+  var str = "";
+for(var j=0; j<i; j++)
+{
+str += "*";
+}
+console.log(str);
+}
+
+console.log("______________________")
+console.log("______________________")
+
+for (var i = 1; i <= 5; i = i + 1) {
+  var str = "";
+  for (var j = 0; j < i; j = j + 1) {
+
+    str += j+1;
+  }
+  console.log(str)
+}
+
+console.log("______________________")
+console.log("______________________")
+
+for(var i=5; i>=1; i--)
+{
+  var str = "";
+for(var j=0; j<i; j++)
+{
+str += j+1;;
+}
+console.log(str);
+}
+
+
